c19a9c1f1f40fd6f104a597505242a5c9217f5ec
CSnowden1 <christopher.snowden@protonmail.com>
2023-01-20 02:38:57 -0500

    added middleware and error handlers

--- backend/controllers/habitController.js
+++ backend/controllers/habitController.js
@@ -1,38 +1,43 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get Habits
 // @route GET /api/goals
 // @access Private
 
-const getHabits = (req, res) => {
+const { async } = require("jshint/src/prod-params");
+
+const getHabits = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "We Got Habits"});
-};
+});
 
 // @desc SET Habit
 // @route POST /api/goals
 // @access Private
 
-const setHabit = (req, res) => {
+const setHabit =  asyncHandler(async  (req, res) => {
     if(!req.body.text) {
-        res.status(400).json({message: 'Please add a text field'});
-    }
+        res.status(400)
+        throw new Error("Please Add A Text Field")
+}
 
     res.status(200).json({ message: "Habit Created"});
-};
+});
 
 // @desc Update Habit
 // @route PUT /api/goals
 // @access Private
 
-const updateHabit = (req, res) => {
+const updateHabit =   asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Habit Updated ${req.params.id}`});
-};
+});
 
 // @desc Delete Habit
 // @route DELETE /api/goals
 // @access Private
 
-const deleteHabit = (req, res) => {
+const deleteHabit =  asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Habit Deleted ${req.params.id}`});
-};
+});
 
 
 
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const  { errorHandler } = require('./Middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(express.json());
@@ -9,6 +9,7 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/api/habits', require('./routes/habitRoutes'));
 
+app.use(errorHandler)
 
 
 
