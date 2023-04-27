ebe53bba0ce1bcf2c8bcaade2d653851ff539377
CSnowden1 <christopher.snowden@protonmail.com>
2023-04-27 13:07:20 -0400

    added overview bar graph

--- client/task-tracker/src/Components/habitProgress/progressBar.js
+++ client/task-tracker/src/Components/habitProgress/progressBar.js
@@ -1,11 +1,14 @@
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
-
-export default function ProgressBar() {
-
-    return <>
-        <div className="progress-bar">
-
-        </div>
-
-    </>
+export default function ProgressBar({ habits }) {
+  return (
+    <div className="progress-bar">
+      <BarChart width={100} height={50} data={habits}>
+        <XAxis dataKey="key" />
+        <YAxis dataKey="count" />
+        <Tooltip />
+        <Bar dataKey="id" fill="gold" barSize={30} />
+      </BarChart>
+    </div>
+  );
 }
