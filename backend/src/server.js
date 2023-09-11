fc0fe3a758d120e260c3fda978a4ac452ee7afda
Chris Snowden <121032172+CSnowden1@users.noreply.github.com>
2023-09-10 20:49:14 -0400

    fixed the json import statement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,50 +1,30 @@
-// server.js
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Habits = require('./Models/habitModel');
 
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const Habits = require('./Models/habitModel')
+// Middleware for parsing JSON
+app.use(express.json());
 
-// routes
+// Routes
+const habitsRoutes = require('./routes/habits'); // Corrected the path here
 
-const habitRoutes = require('./routes/habits');
-
-
-
-
-app.use(express.json);
-
+// Define routes before starting the server
+app.use('/habits', habitsRoutes);
 
 app.get('/', (req, res) => {
-    res.send("Hello NODE API")
-})
-
-app.get('/habits', (req, res) => {
-    res.send("This is the habit data")
-  })
-
-app.post('/habits', async(req, res) => {
-    try {
-        const habit = await Habits.create(req.body)
-        res.status(200).json(habit)
-    } catch(error) {
-        console.log(error.message);
-        res.status(500).json({message: error.message})
-    }
-
-})
-
+    res.send("Hello NODE API");
+});
 
 app.listen(5000, () => {
-    console.log("Node API is runnning on port 5000")
-})
+    console.log("Node API is running on port 5000");
+});
 
-mongoose.
-connect('mongodb+srv://ChiefTweet:94112103@snowdencluster.hjug07l.mongodb.net/User-Habits?retryWrites=true&w=majority')
-.then(() => {
-    console.log("connected to Mongoose")
-}).catch((error) => {
-    console.log(error)
-})
-
-
+mongoose
+    .connect('mongodb+srv://ChiefTweet:94112103@snowdencluster.hjug07l.mongodb.net/User-Habits?retryWrites=true&w=majority')
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
