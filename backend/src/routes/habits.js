dccf9e750145cd02be0c55333e09a8bb6168498f
Chris Snowden <121032172+CSnowden1@users.noreply.github.com>
2023-09-14 16:33:34 -0400

    Revert "fixed the json import statement"
    This reverts commit fc0fe3a758d120e260c3fda978a4ac452ee7afda.

--- backend/src/routes/habits.js
+++ backend/src/routes/habits.js
@@ -1,8 +1,18 @@
+// routes/habits.js
+
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../Middleware/authMiddleware');
+const app = express()
 
-router.get('/habits', (req, res) => {
-  res.send("This is a test route in habits.js");
-});
+
+app.get('/blog', (req, res) => {
+  res.send("Hello NODE API")
+})
+
+//Protected route, only accessible with a valid JWT token
+//router.get('/protected', authMiddleware, (req, res) => {
+//  res.json({ message: 'This is a protected route.' });
+//});
 
 module.exports = router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,50 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const Habits = require('./Models/habitModel');
+// server.js
 
-// Middleware for parsing JSON
-app.use(express.json());
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const Habits = require('./Models/habitModel')
 
-// Routes
-const habitsRoutes = require('./routes/habits'); // Corrected the path here
+// routes
 
-// Define routes before starting the server
-app.use('/habits', habitsRoutes);
+const habitRoutes = require('./routes/habits');
+
+
+
+
+app.use(express.json);
+
 
 app.get('/', (req, res) => {
-    res.send("Hello NODE API");
-});
+    res.send("Hello NODE API")
+})
+
+app.get('/habits', (req, res) => {
+    res.send("This is the habit data")
+  })
+
+app.post('/habits', async(req, res) => {
+    try {
+        const habit = await Habits.create(req.body)
+        res.status(200).json(habit)
+    } catch(error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+
+})
+
 
 app.listen(5000, () => {
-    console.log("Node API is running on port 5000");
-});
+    console.log("Node API is runnning on port 5000")
+})
 
-mongoose
-    .connect('mongodb+srv://ChiefTweet:94112103@snowdencluster.hjug07l.mongodb.net/User-Habits?retryWrites=true&w=majority')
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+mongoose.
+connect('mongodb+srv://ChiefTweet:94112103@snowdencluster.hjug07l.mongodb.net/User-Habits?retryWrites=true&w=majority')
+.then(() => {
+    console.log("connected to Mongoose")
+}).catch((error) => {
+    console.log(error)
+})
+
+
