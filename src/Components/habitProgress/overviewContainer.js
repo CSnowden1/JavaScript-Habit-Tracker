afc26953fd758524288fb8166a9cf91739a7d461
CSnowden1 <snowden.chris@protonmail.com>
2023-10-25 22:35:28 -0400

    Updated the habit overview to be able to see the outline

--- src/Components/habitProgress/overviewContainer.js
+++ src/Components/habitProgress/overviewContainer.js
@@ -1,94 +1,106 @@
-import { Container } from "semantic-ui-react";
-import { useEffect, useState } from "react";
-import ProgressBar from "./progressBar";
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
-import { useAuth } from "../../Context/authContext";
-
-export default function Overview({ habitsChanged, onHabitsChange }) {
-  const { user } = useAuth();
-  const userId = user ? user.user._id : null;
-  const [habits, setHabits] = useState([]);
-
-  async function fetchHabitsFromServer(userId) {
-    try {
-      const response = await fetch(`http://localhost:5000/users/${userId}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (response.ok) {
-        const habits = await response.json();
-        console.log(habits);
-        const habitData = habits.user.habits;
-        return habitData;
-      } else {
-        console.error("Failed to fetch habits from the server");
+import { useState, useEffect } from 'react';
+import { Container } from 'semantic-ui-react';
+  import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+  import { useAuth } from '../../Context/authContext';
+  
+  
+  
+  export default function Overview({ habitsChanged, onHabitsChange }) {
+    const { user } = useAuth();
+    const userId = user ? user.user._id : null;
+    const [habits, setHabits] = useState([]);
+  
+    async function fetchHabitsFromServer(userId) {
+      try {
+        const response = await fetch(`http://localhost:5000/users/${userId}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+  
+        if (response.ok) {
+          const habits = await response.json();
+          console.log(habits);
+          const habitData = habits.user.habits;
+          return habitData;
+        } else {
+          console.error("Failed to fetch habits from the server");
+          return [];
+        }
+      } catch (error) {
+        console.error(error);
         return [];
       }
-    } catch (error) {
-      console.error(error);
-      return [];
     }
-  }
-
-  useEffect(() => {
-    // Define an async function to fetch habits and update the state
-    const fetchAndSetHabits = async () => {
-      const habitsData = await fetchHabitsFromServer(userId);
-      setHabits(habitsData);
-      onHabitsChange();
+  
+    useEffect(() => {
+      // Define an async function to fetch habits and update the state
+      const fetchAndSetHabits = async () => {
+        const habitsData = await fetchHabitsFromServer(userId);
+        setHabits(habitsData);
+        onHabitsChange();
+      };
+  
+      // Check if habitsChanged prop changed, if it did, fetch new habits
+      if (habitsChanged) {
+        fetchAndSetHabits();
+      }
+    }, [habitsChanged, userId, onHabitsChange]);
+  
+    const containerStyles = {
+      background: "rgba(49, 21, 219, .03)",
+      boxShadow: "inset 4px 4px 4px rgba(87, 87, 87, 0.07)",
+      borderRadius: "10px",
+      width: "100vw",
+      height: "10rem",
+      display: "flex",
+      alignContent: "center",
+      alignItems: "center",
+      overflowX: "scroll",
     };
-
-    // Check if habitsChanged prop changed, if it did, fetch new habits
-    if (habitsChanged) {
-      fetchAndSetHabits();
-    }
-  }, [habitsChanged, userId, onHabitsChange]);
-
-  const containerStyles = {
-    background: "rgba(49, 21, 219, .03)",
-    boxShadow: "inset 4px 4px 4px rgba(87, 87, 87, 0.07)",
-    borderRadius: "10px",
-    width: "100vw",
-    height: "10rem",
-    display: "flex",
-    alignContent: "center",
-    alignItems: "center",
-    overflowX: "scroll",
-  };
-
-  return (
-    <>
-      <div style={containerStyles}>
-        <Container
-          style={{
-            width: "auto",
-            border: "solid black 1px",
-            height: "5rem",
-            borderRadius: "1rem",
-            display: "flex",
-            marginLeft: "2rem",
-            marginRight: "2rem",
-          }}
-        >
-          <BarChart width={700} height={100} data={habits}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Bar
-              style={{
-                border: "solid black 2px",
-                backgroundColor: "gold",
-                width: "1rem",
-              }}
-              dataKey="count"
-              fill="gold"
-            />
-          </BarChart>
-        </Container>
-      </div>
-    </>
-  );
-}
+  
+    const renderCustomBar = ({ x, y, width, height, payload}) => {
+      const { value, payload: habits } = payload;
+  
+      const fillPercentage = (habits.count / habits.goal) * 100;
+  
+      return (
+        <g>
+          {/* Outline (border) */}
+          <rect x={x} y={y} width={width} height={height} fill="transparent" stroke="black" strokeWidth="2" />
+          {/* Filled part based on count */}
+          <rect x={x} y={y} width={width * (fillPercentage / 100)} height={height} fill="gold" />
+          <text x={x + width / 2} y={y + height / 2} fill="black" textAnchor="middle" dominantBaseline="middle">
+            {value}
+          </text>
+        </g>
+      );
+    };
+  
+    return (
+      <>
+        <div style={containerStyles}>
+          <Container
+            style={{
+              width: "auto",
+              border: "solid black 1px",
+              height: "5rem",
+              borderRadius: "1rem",
+              display: "flex",
+              marginLeft: "2rem",
+              marginRight: "2rem",
+            }}
+          >
+            <BarChart width={700} height={100} data={habits}>
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="goal" fill="transparent" shape={renderCustomBar} />
+            </BarChart>
+          </Container>
+        </div>
+      </>
+    );
+  }
+  
